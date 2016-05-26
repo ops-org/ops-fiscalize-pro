8d4aca06f60470af79ac020938860e5bd8288d17
Danilo Mendonça <favoratti@gmail.com>
2016-05-25 22:00:44 -0300

    adicionando arquivos faltantes

---
+++ includes/framework/base/BaseController.class.php
@@ -0,0 +1,8 @@
+<?php
+	include_once INCLUDE_ROOT.'/framework/exceptions/ConexaoException.class.php';
+	include_once INCLUDE_ROOT.'/framework/exceptions/SQLException.class.php';
+
+class BaseController {
+	
+}
+?>
